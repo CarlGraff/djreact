5c3f6e1152a63962dea682089981d6792b90b24f
Carl Graff <graffcarl@gmail.com>
2019-05-09 17:03:06 -0700

    Add rest_framework, base React, and Article table

--- Test-Venv.py
+++ Test-Venv.py
@@ -1,5 +1,25 @@
 import sys
+import subprocess
 
+print("==================")
+res = subprocess.check_output(["which", "python"])
+# for line in res.splitlines():
+#     print(line)
+print(res)
 print(sys.version)
 print(sys.executable)
-print("hello")
+print("==================")
+
+
+#print(subprocess.call(["which", "python"])[0])
+# print(sys.version)
+# print(sys.executable)
+# print("hello")
+
+# print("python manage.py runserver")
+# print("python manage.py migrate")
+# print("python manage.py makemigrations")
+# print("python manage.py migrate")
+# print("python manage.py startapp articles")
+# print("python manage.py createsuperuser")
+# seagraph djReact53 double
